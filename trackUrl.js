b81a23476f31a4ed96773595a0d004ae9179902c
Bahroze Ali <hawkeye_sama@protonmail.com>
2020-12-08 18:45:33 +0500

    updated js

--- trackUrl.js
+++ trackUrl.js
@@ -1,20 +1,23 @@
 window.addEventListener("locationchange", function() {
   let location = window.location.href;
-  fetch("https://9hp43o3p3k.execute-api.us-east-2.amazonaws.com/dev/task", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: JSON.stringify({ location: location })
-  })
-    .then(res => {
-      console.log("Request complete! response:");
-      return res.json();
+  let params = new URLSearchParams(window.location.search);
+  let aidVal = params.get("aid");
+  if (aidVal === "00000000") {
+    fetch("https://9hp43o3p3k.execute-api.us-east-2.amazonaws.com/dev/task", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: JSON.stringify({ location: location })
     })
-    .then(data => {
-      console.log(data);
-    });
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        console.log(data);
+      });
+  }
 });
 history.pushState = (f =>
   function pushState() {
